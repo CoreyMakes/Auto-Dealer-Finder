ede2f6f706a9111a95f664ed81e66f7f10abfffc
Yuezhou <lujc515@163.com>
2019-01-24 20:42:54 +0900

    Outlined the style and made the map appear

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var dealers = [
+
+];
+
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: 35.4561941, lng: 139.6309885},
+        zoom: 16,
+    });
+}
